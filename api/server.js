a9036cc40b65bdda463b88949acd516c94ea83e8
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-10-03 12:06:32 -0400

    ready to deploy

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
+/*server.get('/', (req, res) => {
   Shoutouts.find()
   .then(shoutouts => {
     res.status(200).json(shoutouts);
@@ -17,6 +17,17 @@ server.get('/', (req, res) => {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
   });
+});*/
+
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
 });
 
 server.post('/', (req, res) => {
